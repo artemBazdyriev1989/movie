86e011cf1fbd2ea9be6226051e16c4e24f529449
artemBazdyriev1989 <artemka850@gmail.com>
2021-04-29 23:23:24 +0300

    add all files

---
+++ src/components/loader/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+export const Loader = () => {
+  return (
+    <div class="lds-roller">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
